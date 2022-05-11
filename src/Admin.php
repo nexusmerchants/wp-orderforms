f2a11098762102ae58480689a2a12c620fb52a8e
Peter Harlacher <peter@harlacher.co>
2022-05-11 15:49:15 +0700

    Sanitize input

--- src/Admin.php
+++ src/Admin.php
@@ -3,17 +3,14 @@
 namespace OrderForms;
 
 class Admin {
-	private $wpdb, $options;
 	const POST_TYPE = 'orderforms';
 
 	public function __construct() {
-		global $wpdb;
-
-		$this->wpdb = $wpdb;
+		//
 	}
 
 	public static function register_post_type() {
-		$labels  = [
+		$labels = [
 			'name'               => _x( 'Forms', 'post type general name', self::POST_TYPE ),
 			'singular_name'      => _x( 'Form', 'post type singular name', self::POST_TYPE ),
 			'menu_name'          => _x( 'OrderForms', 'admin menu', self::POST_TYPE ),
@@ -30,7 +27,9 @@ class Admin {
 			'not_found_in_trash' => __( 'No Forms found in Trash.', self::POST_TYPE )
 
 		];
+
 		$options = get_option( 'orderforms' );
+
 		register_post_type( self::POST_TYPE, [
 			'labels'              => $labels,
 			'public'              => true,
@@ -60,11 +59,10 @@ class Admin {
 	public function save_meta( $post_id ) {
 		global $post_type;
 
-		if ( $post_type == self::POST_TYPE ) {
-			if ( isset( $_POST['orderforms_id'] ) ) {
-				if ( ! add_post_meta( $post_id, 'orderforms_id', $_POST['orderforms_id'], true ) ) {
-					update_post_meta( $post_id, 'orderforms_id', $_POST['orderforms_id'] );
-				}
+		if ( $post_type === self::POST_TYPE && !empty($_POST['orderforms_id']) ) {
+			$form_url = sanitize_text_field( strtolower( trim( $_POST['orderforms_id'] ) ) );
+			if ( ! add_post_meta( $post_id, 'orderforms_id', $form_url, true ) ) {
+				update_post_meta( $post_id, 'orderforms_id', $form_url );
 			}
 		}
 	}
@@ -83,49 +81,69 @@ class Admin {
 		$data  = get_option( 'orderforms' );
 		$value = get_post_meta( $post->ID, 'orderforms_id', true );
 		?>
-        <div>
-            <label><strong>Form URL:</strong></label><br/>
-            <input style='width:100%' type="text" value="<?php echo $value ?>" name="orderforms_id" size="70" placeholder="https://SUBDOMAIN.orderforms.com/order/form/FORMID" pattern="https://+[A-Za-z0-9]+.orderforms.com/order/form/.+[A-Za-z0-9]"/
-            required>
-        </div>
-        <div style='font-size:12px;margin-top:8px;'>Enter the full URL to the order form</div><?php
+		<div>
+			<label><strong>Form URL:</strong></label><br/>
+			<input style='width:100%' type="text" value="<?php esc_attr_e( $value ) ?>" name="orderforms_id" size="70" autocomplete="off" placeholder="https://SUBDOMAIN.orderforms.com/order/form/FORMID" pattern="https://+[A-Za-z0-9]+.orderforms.com/order/form/.+[A-Za-z0-9]"/
+			required>
+		</div>
+		<div style='font-size:12px;margin-top:8px;'>Enter the full URL to your OrderForms.com form</div><?php
 	}
 
 	public function admin_settings_nav() {
-		add_submenu_page( 'edit.php?post_type=' . self::POST_TYPE, 'OrderForms Settings', __( 'Settings' ), 'manage_options', 'settings', [
-			$this,
-			'settings_page'
-		] );
+		add_submenu_page( 'edit.php?post_type=' . self::POST_TYPE, 'OrderForms Settings', __( 'Settings' ),
+			'manage_options', 'settings', [
+				$this,
+				'settings_page'
+			] );
 	}
 
 	public function settings_page() {
+		$updated = $error = false;
 		$options = get_option( 'orderforms' );
+
 		if ( ! empty( $_POST['_orderforms_wpnonce'] ) && wp_verify_nonce( $_POST['_orderforms_wpnonce'] ) ) {
-			update_option( 'orderforms', $_POST['orderforms'] );
-			$options = $_POST['orderforms'];
-			$updated = true;
+			$new_options = array_map('sanitize_text_field', $_POST['orderforms']);
+
+			if ( empty( trim( $new_options['slug'] ) ) ) {
+				$error = true;
+			}
+
+			if ( ! $error ) {
+				update_option( 'orderforms', $new_options );
+				$options = $new_options;
+				$updated = true;
+			}
 		}
 		?>
-        <div class="wrap">
-        <h1><?php echo get_admin_page_title(); ?></h1>
+		<div class="wrap">
+		<h1><?php echo get_admin_page_title(); ?></h1>
 
-		<?php if ( ! empty( $updated ) ): ?>
-            <div class="notice notice-success">
-                <p><?php _e( 'Settings Saved!' ); ?></p>
-            </div>
+		<?php if ( $updated ): ?>
+			<div class="notice notice-success">
+				<p><?php _e( 'Settings Saved!' ); ?></p>
+			</div>
 		<?php endif; ?>
 
-        <form action="" method="post" accept-charset="utf-8">
+		<?php if ( $error ): ?>
+			<div class="notice notice-error">
+				<p><?php _e( 'Please enter a slug' ); ?></p>
+			</div>
+		<?php endif; ?>
+
+		<form action="" method="post" accept-charset="utf-8">
 			<?php wp_nonce_field( - 1, '_orderforms_wpnonce' ); ?>
-            <div>
-                <label>Enter Slug Name:</label><br/>
+			<div>
+				<label>Enter Slug Name:</label><br/>
 				<?php echo get_site_url(); ?>
-                /<input type="text" name="orderforms[slug]" value="<?php echo $options['slug'] ?? '' ?>">/formname
-            </div>
-            <div style='font-size:12px;margin-top:8px;'><strong>Note:</strong> After changing the slug please <a href="/wp-admin/options-permalink.php">update the "Permalink Settings" by clicking on the "Save Changes" button</a>.</div>
+				/<input type="text" name="orderforms[slug]" value="<?php esc_attr_e( $options['slug'] ?? '' ) ?>">/formname
+			</div>
+			<div style='font-size:12px;margin-top:8px;'><strong>Note:</strong> After changing the slug please
+				<a href="/wp-admin/options-permalink.php">update the "Permalink Settings" by clicking on the "Save
+					Changes" button</a>.
+			</div>
 
 			<?php submit_button(); ?>
-        </form>
-        </div><?php
+		</form>
+		</div><?php
 	}
 }
